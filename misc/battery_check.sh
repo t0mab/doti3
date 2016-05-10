7c1e0cfc0e81955c435a5cb5fe5568c6a8bcf268
Thomas Baguet <baguet@unistra.fr>
2016-05-10 10:04:48 +0200

    fix bad indentation of battery_check script

--- misc/battery_check.sh
+++ misc/battery_check.sh
@@ -12,7 +12,7 @@ NOTIFYSEND=/usr/bin/notify-send
 
 if [ ! -x $NOTIFYSEND ]
 then
-   exit 0
+    exit 0
 fi
 
 # check every $CHECK seconds
@@ -20,35 +20,35 @@ CHECK=60
 
 ( while true
 do
-   BATSTATE=`cat ${BATDIR}/status`
-   # Note valid BATSTATE values are: Charged, Charging, and Discharging
-   if [ "$BATSTATE" = "Discharging" ]
-   then
-      REM_CAP=`cat ${BATDIR}/charge_now`
-      FULL_CAP=`cat ${BATDIR}/charge_full`
-      CHARGE=`echo $(( $REM_CAP * 100 / $FULL_CAP ))`
-      if [ "$CHARGE" -lt "5" ]
-      then
-        systemctl suspend
-      fi
-      if [ "$CHARGE" -lt "25" ]
-      then
-	    $NOTIFYSEND -i $ICONPATH/battery_critical.png -u critical -t 30000 "battery critical" "Shutdown or connect power now"
-	  sleep $CHECK
-	 continue
-      fi
-      if [ "$CHARGE" -lt "30" ]
-      then
-	    $NOTIFYSEND -i $ICONPATH/battery_low.png -u normal -t 20000 "battery low" "Connect power soon"
-      fi
-      sleep $CHECK
-   elif [ "$BATSTATE" = "Charged" ]
-   then
-      $NOTIFYSEND -i $ICONPATH/battery_full.png -u normal -t 20000 "battery fully charged" "Disconnect from power"
-      sleep $CHECK
-   else
-      sleep $CHECK
-   fi
+    BATSTATE=`cat ${BATDIR}/status`
+    # Note valid BATSTATE values are: Charged, Charging, and Discharging
+    if [ "$BATSTATE" = "Discharging" ]
+    then
+        REM_CAP=`cat ${BATDIR}/charge_now`
+        FULL_CAP=`cat ${BATDIR}/charge_full`
+        CHARGE=`echo $(( $REM_CAP * 100 / $FULL_CAP ))`
+        if [ "$CHARGE" -lt "5" ]
+        then
+            systemctl suspend
+        fi
+        if [ "$CHARGE" -lt "25" ]
+        then
+            $NOTIFYSEND -i $ICONPATH/battery_critical.png -u critical -t 30000 "battery critical" "Shutdown or connect power now"
+            sleep $CHECK
+            continue
+        fi
+        if [ "$CHARGE" -lt "30" ]
+        then
+            $NOTIFYSEND -i $ICONPATH/battery_low.png -u normal -t 20000 "battery low" "Connect power soon"
+        fi
+        sleep $CHECK
+    elif [ "$BATSTATE" = "Charged" ]
+    then
+        $NOTIFYSEND -i $ICONPATH/battery_full.png -u normal -t 20000 "battery fully charged" "Disconnect from power"
+        sleep $CHECK
+    else
+        sleep $CHECK
+    fi
 done
 ) &
 
