3ffe8e1a4ce3afa5cc145116f150994f7a4c24c1
Thomas Baguet <baguet@unistra.fr>
2018-02-07 01:12:17 +0100

    Fix whitespaces in py script

--- misc/windowname.py
+++ misc/windowname.py
@@ -19,31 +19,34 @@ i3 = i3ipc.Connection()
 
 
 def truncate_name(name):
-	name = name.replace("&", "+")
-	truncated = name[:40] + (name[40:] and '...')
-	return filter(lambda x: x in set(string.printable), truncated)
+    name = name.replace("&", "+")
+    truncated = name[:40] + (name[40:] and '...')
+    return filter(lambda x: x in set(string.printable), truncated)
 
 
 def on_event(i3, e):
-	if e.change in ['focus', 'title']:
-		tree = i3.get_tree()
-		window = tree.find_focused();
-		if window != None:
-			new_name = window.name
-			workspace = window.workspace()
-			if workspace != None:		
-				current_num = workspace.num
-				current_name = workspace.name
-				for ws in tree.workspaces():
-					current_name = ws.name					
-					if ws.num != current_num:
-						new_name = workspace_defaults[ws.num]
-					else:	
-						if window.name != None:
-							new_name = "%d:%s" % (current_num, truncate_name(window.name))
-						else:
-							new_name = workspace_defaults[ws.num]
-					i3.command("rename workspace \"%s\" to \"%s\"" % (current_name, new_name))
+    if e.change in ['focus', 'title']:
+        tree = i3.get_tree()
+        window = tree.find_focused()
+        if window != None:
+            new_name = window.name
+            workspace = window.workspace()
+            if workspace != None:
+                current_num = workspace.num
+                current_name = workspace.name
+                for ws in tree.workspaces():
+                    current_name = ws.name
+                    if ws.num != current_num:
+                        new_name = workspace_defaults[ws.num]
+                    else:
+                        if window.name != None:
+                            new_name = "%d:%s" % (
+                                current_num, truncate_name(window.name))
+                        else:
+                            new_name = workspace_defaults[ws.num]
+                    i3.command("rename workspace \"%s\" to \"%s\"" % (current_name, new_name))
+
+
 i3.on('workspace', on_event)
 i3.on('window', on_event)
 
